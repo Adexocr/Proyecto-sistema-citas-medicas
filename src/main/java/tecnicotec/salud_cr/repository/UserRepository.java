dc9d3c9c6c684e3b2470c7e345c2b55ccdc8f802
Kadem <kxtarx000@gmail.com>
2026-06-25 23:16:54 -0600

    Correccion de error en UserRepository

--- src/main/java/tecnicotec/salud_cr/repository/UserRepository.java
+++ src/main/java/tecnicotec/salud_cr/repository/UserRepository.java
@@ -16,7 +16,7 @@ public interface UserRepository extends CrudRepository<User, Long> {
     @Query("SELECT * FROM users WHERE user_name = :username")
     Optional<User> findByUsername(String username);
 
-    @Query("SELECT * FROM users WHERE email = ;email")
+    @Query("SELECT * FROM users WHERE email = :email")
     Optional<User> findByEmail(String email);
 }
 
