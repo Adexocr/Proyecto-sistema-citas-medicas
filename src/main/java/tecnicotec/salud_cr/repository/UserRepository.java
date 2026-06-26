6926345d5efc701f4c74384e18ada32dbcacc38e
Kadem <kxtarx000@gmail.com>
2026-06-25 22:04:03 -0600

    Añadido a user repository findbyusername y findbyemail

--- src/main/java/tecnicotec/salud_cr/repository/UserRepository.java
+++ src/main/java/tecnicotec/salud_cr/repository/UserRepository.java
@@ -1,11 +1,22 @@
 package tecnicotec.salud_cr.repository;
 
-
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import tecnicotec.salud_cr.data.User;
+
+import java.util.Optional;
+
 
 /**
  * Repository interface for User-related operations.
  * Implementations will be provided later (in-memory, JPA, etc.).
  */
-public interface UserRepository extends CrudRepository<User, Long> {}
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    @Query("SELECT * FROM users WHERE user_name = :username")
+    Optional<User> findByUsername(String username);
+
+    @Query("SELECT * FROM users WHERE email = ;email")
+    Optional<User> findByEmail(String email);
+}
+
